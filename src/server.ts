d9a0801a6884a8561cdd3f6fe8e5996f65b13422
khattab <khatabhasan700@gmail.com>
2019-08-20 22:27:30 +0200

    Build the front

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
     // tslint:disable:no-console
 const server = app.listen(port, () => {
     console.log(`Listing on port ${port}`);
-}).on("error", (e) => {
+}).on("error", (e: any) => {
     console.error(e);
     process.exit(1);
 });
